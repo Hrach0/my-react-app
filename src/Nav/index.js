4ee16393cb1fda07395e09d1c930655e8610e8e2
Hrach <avetisyanhrach12@gmail.com>
2023-12-02 20:09:08 +0400

    first commit

---
+++ src/Nav/index.js
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+function Nav(){
+    return(
+        <div classNameName="Nav">
+            <nav>
+<a href="#menu1-1">HOME</a><a href="#menu1-2">MENU</a><a href="#menu1-3">ABOUT</a><a href="#menu1-4">CONTACT</a>
+</nav>
+        </div>
+    )
+}
+
+export default Nav;
